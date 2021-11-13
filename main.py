4f47290967713636f0ae5ca368dba7908b5863a0
ABHISHEK BHARDWAJ <59165472+abhishekbhardwaj2023@users.noreply.github.com>
2021-11-14 01:22:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,9 +12,12 @@ def generate_password():
     letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
                'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
                'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    
     numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    
     symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
+    
     password_letters = [choice(letters) for _ in range(randint(8, 10))]
     password_symbols = [choice(symbols) for _ in range(randint(2, 4))]
     password_numbers = [choice(numbers) for _ in range(randint(2, 4))]
@@ -61,7 +64,7 @@ def save():
             password_entry.delete(0, END)
 
 
-# ---------------------------- FIND PASSWORD ------------------------------- #
+# ---------------------------- FIND PASSWORD (Exception handling)------------------------------- #
 def find_password():
     website = website_entry.get()
     try:
