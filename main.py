8b41941d80d70340ad4db48306a0e7ef6d7ce65b
ABHISHEK BHARDWAJ <59165472+abhishekbhardwaj2023@users.noreply.github.com>
2022-01-06 23:56:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,10 +5,8 @@ from random import choice, randint, shuffle
 import pyperclip
 import json
 
-
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
-# Password Generator Project
 def generate_password():
     letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
                'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
